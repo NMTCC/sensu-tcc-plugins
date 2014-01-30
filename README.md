52a2f690fb3577682021ce0d18328382b321ad22
floomby <floomby@nmt.edu>
2014-01-30 11:35:20 -0700

    installer and basic layout of repo

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Sensu Plugins
+
+
+Instructions
+
+Clone the plugin repo
+  git clone ssh://gitolite@inception/sensu-plugins
+
+If sensu client is not installed (run as root)
+  ./install-sensu-client-fedora.sh
+
+
+The plugins directory contains the sud-dirs with the channel plugins
---
+++ install-sensu-client-fedora.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# this install the sensu client and plugins on a fedora machine
+
+# add repo and install sensu
+cp config/sensu.repo /etc/yum.repos.d/
+yes | yum install sensu
+
+# configure the machine
+mkdir -p /etc/sensu/conf.d/
+cp config/client.json /etc/sensu/conf.d/
+sed -i /etc/sensu/conf.d/client.json -e s/NAME/`hostname | cut -f 1 -d"."`/
+cp config/config.json /etc/sensu/
+echo Enter the database password
+read DB_PASS
+sed -i /etc/sensu/config.json -e s/DBPASS/"$DB_PASS"/
+
+# copy the plugins
+cp -r plugins/fedora/ /etc/sensu/plugins/
+
+# fix the permisions/ownership
+chown sensu `find /etc/sensu/ | xargs file | grep -v directory | cut -f 1 -d":"`
