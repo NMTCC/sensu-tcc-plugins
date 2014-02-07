72528273cbd5cf12c937663eabb3787da1f1215e
Anthony Baca <kaiba@nmt.edu>
2014-02-07 14:06:11 -0700

    Changed name of class of users metric

--- plugins/system/users_metrics.rb
+++ plugins/system/users_metrics.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 # Plugin that monitors how many users are logged into a machine
-# Simply returns the amount of users logged on at any time
+# Returns the amount of users logged on at any time
 
 require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-plugin/metric/cli'
 require 'socket'
 
-class ConnectionSpeedGraphite < Sensu::Plugin::Metric::CLI::Graphite
+class UsersLoggedInGraphite < Sensu::Plugin::Metric::CLI::Graphite
 
 	option :scheme,
     :description => "Metric naming scheme, text to prepend to metric",
@@ -17,7 +17,7 @@ class ConnectionSpeedGraphite < Sensu::Plugin::Metric::CLI::Graphite
 	def run
 		timestamp = Time.now.to_i
 		who_output = `who`
-		users_number = who_output.scan(/^\w+/).size
+		users_number = who_output.scan(/^\w/).size
 		output "#{config[:scheme]}", users_number, timestamp
 		ok
 	end
