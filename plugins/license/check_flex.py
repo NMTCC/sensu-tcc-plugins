18df29feb46a1b8324db4acaf8513bdd05b5d4cd
Anthony Baca <kaiba@nmt.edu>
2014-02-28 12:20:06 -0700

    Fixed some bugs in flex sensu plugins

--- plugins/license/check_flex.py
+++ plugins/license/check_flex.py
@@ -15,31 +15,16 @@ parser.add_argument('--hosts', nargs='+', help="fqdn names of license servers to
 parser.add_argument('--lmpath', nargs='?', help="path to lmutil program")
 args = parser.parse_args()
 
-#Generate License File(s)
-files = []
-for host in args.hosts:
-    t = tempfile.NamedTemporaryFile()
-    data = "SERVER " + host + " 27000\nUSE_SERVER"
-    t.write(data)
-    t.flush()
-    files.append(t)
-
 #Change Path to lmutil program
 if args.lmpath:
     lmutil = args.lmpath
 else:
     lmutil = '/usr/local/matlab-2013a/etc/glnxa64/lmutil'
-
 #Make sure path correctly points to a valid lmutil
 (statuscheck, output) = commands.getstatusoutput(lmutil)
 if statuscheck != 0:
     print "Please enter a valid path to lmutil"
     sys.exit(1)
-
-#Run lmstat (with lmutil) on each of the license files
-output = ''
-for licfile in files:
-    output = output + commands.getoutput(lmutil + " lmstat -a -c " + licfile.name)
 
 #Create Pattern were looking for
 #Regex Groups 
@@ -48,20 +33,35 @@ for licfile in files:
 #Group 5: # Licenses in use
 pattern = re.compile('(Users of )([^:]*)(:  \(Total of )([0-9]*)( licenses issued;  Total of )([0-9]*)( licenses in use\))')
 
-#Perform the search on both output arrays (one from each command) and store matches in list
-matches = re.findall(pattern, output)
+#Check all the hosts
+if args.hosts:
+    for host in args.hosts:
+        #Generate License File
+        t = tempfile.NamedTemporaryFile()
+        data = "SERVER " + host + " 27000\nUSE_SERVER"
+        t.write(data)
+        t.flush()
 
-#Parse each individual match for errors
-for match in matches:
-    ratio = float(match[5]) / float(match[3])
-    if ratio == 1:
-        print "All " + match[1] + " licenses checked out"
-        sys.exit(2)
-    if ratio > threshold:
-        print match[1] + " licenses are in high use"
-        sys.exit(1)
+        #Run lmstat (with lmutil) on each of the license files
+        output = commands.getoutput(lmutil + " lmstat -a -c " + t.name)
 
+        #Perform the search and store matches in list
+        matches = re.findall(pattern, output)
 
+        #Parse each individual match for errors
+        for match in matches:
+            ratio = float(match[5]) / float(match[3])
+            if ratio == 1:
+                print "All " + match[1] + " licenses checked out"
+                sys.exit(2)
+            if ratio > threshold:
+                print match[1] + " licenses are in high use"
+                sys.exit(1)
+else:
+    print "Please specify hosts to check with the --hosts option"
+    sys.exit(2)
+
+#All checks have been passed
 print "Licenses are A-OK"
 sys.exit(0)
 
