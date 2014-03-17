05eebf925b4642cea72e559748e8180587c4ad16
Anthony Baca <kaiba@nmt.edu>
2014-03-17 10:47:59 -0600

    Separated site specific info and code for hubotpush handler

--- plugins/handlers/hubotpush.rb
+++ plugins/handlers/hubotpush.rb
@@ -1,20 +1,36 @@
 #!/usr/bin/env ruby
+#
+# Pushes Sensu Reports to a listening Hubot
+#
+# This Handler sends client name, check name, status code, and check 
+# output to a properly configured Hubot HTTP listener.
+#
+# Data is sent though an HTTP post.
+#
+# One setting is required in hubotpush.json
+#   host : The ip and port of the Hubot HTTP listener.
 
-require 'rubygems'
+require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'json'
 require 'net/http'
 
-#Read even data
-#testjson = '{"client":{"name":"phobos"}, "check":{"name":"process_check","status":0,"output":"bad stuff"}}'
-event = JSON.parse(STDIN.read, :symbolize_names => true)
+class HubotPush < Sensu::Handler
 
-#Extract viable information
-client = "#{event[:client][:name]}"
-check = "#{event[:check][:name]}"
-statuscode = "#{event[:check][:status]}"
-output = "#{event[:check][:output]}"
+  def widget
+    @event['client']['name'] + '-' + @event['check']['name']
+  end
 
-#Post Data to Hubot Listener
-uri = URI("http://neptr.nmt.edu:8080/hubot/sensu")
-response = Net::HTTP.post_form(uri, {'client'=>client,'check'=>check,'statuscode'=>statuscode,'output'=>output})
+  def handle
+    #Extract viable information
+    client = @event['client']['name']
+    check = @event['client']['name']
+    statuscode = @event['client']['name']
+    output = @event['client']['name']
+
+    #Post Data to Hubot Listener
+    uri = URI(settings['hubotpush']['host'])
+    response = Net::HTTP.post_form(uri, {'client'=>client,'check'=>check,'statuscode'=>statuscode,'output'=>output})
+  end
+end
+
 
