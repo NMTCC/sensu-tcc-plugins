e2aaf9c44d6b7b8172cd5104f257535dfe313363
Anthony Baca <kaiba@nmt.edu>
2014-04-16 11:38:47 -0600

    Changed handler hubotpush.rb again to inherit sensu-plugin Handler class

--- plugins/handlers/hubotpush.rb
+++ plugins/handlers/hubotpush.rb
@@ -13,21 +13,21 @@
 require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'json'
 require 'net/http'
+require 'sensu-handler'
 
-#Read user settings from hubotpush.json
-settingsfile = "#{File.expand_path(File.dirname(__FILE__))}" + "/hubotpush.json"
-settings = JSON.parse(IO.read(settingsfile), :symbolize_names => true)
+class HubotPush < Sensu::Handler
 
-# Read event data
-event = JSON.parse(STDIN.read, :symbolize_names => true)
+  def handle
+    #Extract viable information
+    client = @event['client']['name']
+    check = @event['check']['name']
+    statuscode = @event['check']['status']
+    output = @event['check']['output']
 
-#Extract viable information
-client = "#{event[:client]}"
-check = "#{event[:check][:name]}"
-statuscode = "#{event[:check][:status]}"
-output = "#{event[:check][:output]}".strip
+    #Post Data to Hubot Listener
+    uri = URI(settings['hubotpush']['host'])
+    response = Net::HTTP.post_form(uri, {'client'=>client,'check'=>check,'statuscode'=>statuscode,'output'=>output})
+  end
+end
 
-#Post Data to Hubot Listener
-uri = URI("#{settings[:hubotpush][:host]}")
-response = Net::HTTP.post_form(uri, {'client'=>client,'check'=>check,'statuscode'=>statuscode,'output'=>output})
 
