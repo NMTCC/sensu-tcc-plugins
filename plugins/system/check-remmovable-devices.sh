86882df3f3ea5fd559c6a075766eb0535d897074
Anthony Baca <kaiba@nmt.edu>
2014-05-28 09:08:24 -0600

    Check-remmovable-devices checks if user is logged in

--- plugins/system/check-remmovable-devices.sh
+++ plugins/system/check-remmovable-devices.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# return 0 for no device and 1 for a attached device
+# return 0 for no device (or if users logged in) and 1 for a attached device
 echo "$(hostname).usb.remmovable"
 
+#Check for logged in users
+if [[ -n $(users) ]]
+then
+	exit 0
+fi
+
+#Check for USB mass storage
 if ! [ $(lsusb -v 2>/dev/null | grep -i binterfaceclass | grep -i mass | wc -l) == 0 ]
 then
 	exit 1	
